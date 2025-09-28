ac8ca892a95a4bffcd2ef37e5e3a7d8d6572a371
Fadhil Ramadhan <97286230+fadhilramadhan25@users.noreply.github.com>
2025-09-28 22:44:21 +0700

    Update index.tsx

--- src/features/SearchPage/index.tsx
+++ src/features/SearchPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import type { Movie } from '../../services/movie';
 import { searchMovies } from '../../services/api';
