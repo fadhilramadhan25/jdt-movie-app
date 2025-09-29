b6069f4378875b30e4ff9df65b6e13445d9b704a
JDT16 Muhammad Fadhilah Faqih <jdt16.muhammad.faqih@gmail.com>
2025-09-29 09:08:34 +0700

    fixing

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,28 +1,28 @@
-import { Outlet, useLocation } from "react-router";
-import { useToken } from "../hooks/useToken";
+// import { Outlet, useLocation } from "react-router";
+// import { useToken } from "../hooks/useToken";
 
 
-const ProtectedRoutes = () => {
-  const { pathname } = useLocation();
+// const ProtectedRoutes = () => {
+//   const { pathname } = useLocation();
 
-  const { user } = useToken();
+//   const { user } = useToken();
 
-  const tokenProtected = ["/", "/detail", "/product"];
-  const auth = ["/login"];
+//   const tokenProtected = ["/", "/detail", "/product"];
+//   const auth = ["/login"];
 
-  if (tokenProtected.includes(pathname)) {
-    if (!user?.accessToken) {
-      window.location.href = "/login";
-    }
-  }
+//   if (tokenProtected.includes(pathname)) {
+//     if (!user?.accessToken) {
+//       window.location.href = "/login";
+//     }
+//   }
 
-  if (auth.includes(pathname)) {
-    if (user?.accessToken) {
-      window.location.href = "/";
-    }
-  }
+//   if (auth.includes(pathname)) {
+//     if (user?.accessToken) {
+//       window.location.href = "/";
+//     }
+//   }
 
-  return <Outlet />;
-};
+//   return <Outlet />;
+// };
 
-export default ProtectedRoutes;
+// export default ProtectedRoutes;
